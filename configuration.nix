8b705738198e2e55581138a71fe13c622f339310
cjenks <me@connorjenks.com>
2024-12-20 11:26:56 -0500

    added pkgs.util-linux

--- configuration.nix
+++ configuration.nix
@@ -68,6 +68,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     pkgs.htop-vim
     pkgs.cowsay
+    pkgs.util-linux
   #  wget
   ];
   
