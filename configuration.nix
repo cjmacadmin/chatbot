fd582bdafa312633d7f9af29a50e31c5ebea2cf0
cjenks <me@connorjenks.com>
2024-12-20 10:33:08 -0500

    added doas as a system package??

--- configuration.nix
+++ configuration.nix
@@ -67,6 +67,7 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     pkgs.htop-vim
+    pkgs.doas
   #  wget
   ];
   
