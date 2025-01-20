35efd1f9de18c37f3db7db580f50b7b9db3bfd88
cjmacadmin <me@connorjenks.com>
2025-01-20 13:23:02 -0500

    fix

--- runners.nix
+++ runners.nix
@@ -1,34 +1,34 @@
 { config, lib, pkgs, ... }:
-{ 
+{
   services.github-runners = {
-#     runner = {
-#       enable = true;
-#       url = "https://github.com/tylercote/gnc-telegram-bot";
-#       tokenFile = "/etc/gnc_bot/github_token";
-#       replace = true;
-#       extraLabels = [ "nixos" "self-hosted" ];
-      
-#       # Configure the runner environment
-#       serviceOverrides = {
-#         User = "github-runner";
-#         WorkingDirectory = lib.mkForce "/var/lib/github-runner";
-#       };
+    runner = {
+      enable = true;
+      url = "https://github.com/cjmacadmin/gnc-telegram-bot";
+      tokenFile = "/etc/gnc_bot/github_token";
+      replace = true;
+      extraLabels = [ "nixos" "self-hosted" ];
 
-#       # Runner configuration
-#       extraPackages = with pkgs; [
-#         # Common build tools
-#         git
-#         nixFlakes
-#         curl
-#         wget
-#         gnumake
-#         gcc
-#         docker
-#         docker-compose
-#         docker-buildx
-#       ];
-#     };
+      # Configure the runner environment
+      serviceOverrides = {
+        User = "github-runner";
+        WorkingDirectory = lib.mkForce "/var/lib/github-runner";
+      };
 
+      # Runner configuration
+      extraPackages = with pkgs; [
+        # Common build tools
+        git
+        nixFlakes
+        curl
+        wget
+        gnumake
+        gcc
+        docker
+        docker-compose
+        docker-buildx
+        sudo
+      ];
+    };
     nixedit = {
       enable = true;
       url = "https://github.com/cjmacadmin/chatbot";
@@ -53,6 +53,7 @@
         docker
         docker-compose
         docker-buildx
+        sudo
       ];
     };
   };
